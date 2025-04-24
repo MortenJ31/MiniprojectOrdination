0311ec834a3ac9218b22dad669a50def417f1866
stiners <160128882+stiners@users.noreply.github.com>
2025-04-24 11:24:42 +0200

    Implemented doegnDosis() in DagligSkæv.cs. Idk if it needs to check the date?

--- shared/Model/DagligSkæv.cs
+++ shared/Model/DagligSkæv.cs
@@ -22,8 +22,7 @@ public class DagligSkæv : Ordination {
 	}
 
 	public override double doegnDosis() {
-		// TODO: Implement!
-        return -1;
+		return doser.Sum(d => d.antal); // Does this need to check the date somehow??
 	}
 
 	public override String getType() {
