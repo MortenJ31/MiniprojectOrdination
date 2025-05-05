89109a125dab4f3d788d66d4d19aeaba2d6abc02
stiners <160128882+stiners@users.noreply.github.com>
2025-05-05 22:03:15 +0200

    Change.

--- shared/Model/DagligSkæv.cs
+++ shared/Model/DagligSkæv.cs
@@ -21,8 +21,15 @@ public class DagligSkæv : Ordination {
 		return base.antalDage() * doegnDosis();
 	}
 
-	public override double doegnDosis() {
-		return doser.Sum(d => d.antal); // Does this need to check the date somehow??
+	public override double doegnDosis()
+	{
+		var doegnDosis = doser.Sum(d => d.antal);
+		
+		if (doegnDosis <= 0)
+		{
+			throw new ArgumentException("Samlet dosis kan ikke være <= 0");
+		}
+		return doegnDosis;
 	}
 
 	public override String getType() {
