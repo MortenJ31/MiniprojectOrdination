b34fb9cebcf33ad397dc58934ae4aac28275e8d4
Andreas Christensen <Andreasdebel@hotmail.com>
2025-05-05 10:20:21 +0200

    ORdination test

--- ordination-test/ServiceTest.cs
+++ ordination-test/ServiceTest.cs
@@ -67,4 +67,49 @@ public class ServiceTest
         var result = service.GetAnbefaletDosisPerDøgn(1, 1);
         Assert.IsNotNull(result);
     }
+
+    [TestMethod]
+    public void TestOrdinationAntalDage()
+    {
+        Laegemiddel lm = service.GetLaegemidler().First();
+        Ordination ordination = new PN(new DateTime(2025, 5, 1), new DateTime(2025, 5, 1), 123, lm);
+        
+        // TC 1-4 (PN)
+        Assert.AreEqual(1, ordination.antalDage());
+
+        ordination = new PN(new DateTime(2025, 5, 1), new DateTime(2025, 5, 5), 123, lm);
+        Assert.AreEqual(5, ordination.antalDage());
+
+        ordination = new PN(new DateTime(2025, 5, 1), new DateTime(2025, 5, 30), 123, lm);
+        Assert.AreEqual(30, ordination.antalDage());
+
+        ordination = new PN(new DateTime(2025, 5, 1), new DateTime(2025, 4, 1), 123, lm);
+        Assert.ThrowsException<ArgumentException>(() => ordination.antalDage());
+
+        // TC 5-8
+        ordination = new DagligFast(new DateTime(2025, 5, 1), new DateTime(2025, 5, 1), lm, 2, 0, 1, 0);
+        Assert.AreEqual(1, ordination.antalDage());
+
+        ordination = new DagligFast(new DateTime(2025, 5, 1), new DateTime(2025, 5, 5), lm, 2, 0, 1, 0);
+        Assert.AreEqual(5, ordination.antalDage());
+
+        ordination = new DagligFast(new DateTime(2025, 5, 1), new DateTime(2025, 5, 30), lm, 2, 0, 1, 0);
+        Assert.AreEqual(30, ordination.antalDage());
+
+        ordination = new DagligFast(new DateTime(2025, 5, 1), new DateTime(2025, 4, 1), lm, 2, 0, 1, 0);
+        Assert.ThrowsException<ArgumentException>(() => ordination.antalDage());
+
+        // TC 9-12
+        ordination = new DagligSkæv(new DateTime(2025, 5, 1), new DateTime(2025, 5, 1), lm);
+        Assert.AreEqual(1, ordination.antalDage());
+
+        ordination = new DagligSkæv(new DateTime(2025, 5, 1), new DateTime(2025, 5, 5), lm);
+        Assert.AreEqual(5, ordination.antalDage());
+
+        ordination = new DagligSkæv(new DateTime(2025, 5, 1), new DateTime(2025, 5, 30), lm);
+        Assert.AreEqual(30, ordination.antalDage());
+
+        ordination = new DagligSkæv(new DateTime(2025, 5, 1), new DateTime(2025, 4, 1), lm);
+        Assert.ThrowsException<ArgumentException>(() => ordination.antalDage());
+    }
 }
